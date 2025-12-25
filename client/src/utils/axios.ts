bd35acfea09073979116c690d574287a0bae17cc
Sallal Zahid <none87328@gmail.com>
2025-12-25 20:19:43 +0500

    Add frontend source files

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
-// Automatically attach JWT token to every request
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
